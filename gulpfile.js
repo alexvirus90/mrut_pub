da2360db73cf02b172a97f3eb40d95c82998706b
a.komarov <a.kom90@mail.ru>
2017-10-09 18:31:44 +0300

    Added some changes in file gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,33 @@
 'use strict';
 
-var gulp = require('gulp');
+const gulp        = require('gulp'),
+      sass        = require('gulp-sass'),
+      browserSync = require('browser-sync');
 
-gulp.task('my', function () {
-  return gulp.src('source-files')
-  .pipe()
+gulp.task('sass', function () {
+  return gulp.src('app/sass/**/*.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('html', function () {
+  return gulp.src('app/index.html')
+    .pipe(gulp.dest('dist'))
+	.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function () {
+  browserSync({
+    server: {
+      baseDir: 'dist'
+    },
+    notify: false
+  });
+});
+
+gulp.task('watch', ['browser-sync', 'sass', 'html'], function () {
+  gulp.watch('app/sass/**/*.sass', ['sass']);
+  gulp.watch('app/*.html', ['html'], browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
 });
